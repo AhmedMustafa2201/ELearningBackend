8a6b659cc5ab9e864bc6243a033f6b86343996e2
abdullah415 <abdullahhamdo104@gmail.com>
2021-08-02 16:28:01 +0200

    Add signalR

---
+++ ELearningBackend/Models/IHubClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELearningBackend.Models
+{
+    public interface IHubClient
+    {
+        Task broadcast();
+    }
+}
