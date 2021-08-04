23974a96066f9f681208224ac2ff86008ff658a0
hsn656 <hassan.mhmd656@gmail.com>
2021-08-04 02:16:40 -1000

    seed Course Images

--- ELearningBackend/Migrations/20210804121504_addDiscriptionToArticle.Designer.cs
+++ ELearningBackend/Migrations/20210804121504_addDiscriptionToArticle.Designer.cs
@@ -4,14 +4,16 @@ using ELearningBackend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ELearningBackend.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210804121504_addDiscriptionToArticle")]
+    partial class addDiscriptionToArticle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
